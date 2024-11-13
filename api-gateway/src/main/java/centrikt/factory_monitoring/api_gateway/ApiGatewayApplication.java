6c154f4b3092de5a070f5292e0463350d1fbb8f7
Andrey <stg51@inbox.ru>
2024-11-13 17:58:53 +0300

    add config, eureka server

--- api-gateway/src/main/java/centrikt/factory_monitoring/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/centrikt/factory_monitoring/api_gateway/ApiGatewayApplication.java
@@ -2,14 +2,9 @@ package centrikt.factory_monitoring.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
-@SpringBootApplication(scanBasePackages = "centrikt.factory-monitoring")
+@SpringBootApplication
 public class ApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
