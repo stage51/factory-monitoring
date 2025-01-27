93a0a4187c3a0f884df7475113e39cb7baf63e76
stage51 <sadzillaxxx@gmail.com>
2025-01-27 18:05:09 +0300

    fix filters for position services

--- five-minute-report/src/main/java/centrikt/factory_monitoring/five_minute_report/services/impl/PositionServiceImpl.java
+++ five-minute-report/src/main/java/centrikt/factory_monitoring/five_minute_report/services/impl/PositionServiceImpl.java
@@ -160,7 +160,7 @@ public class PositionServiceImpl implements PositionService {
         log.trace("Entering getPage method with size: {}, number: {}, sortBy: {}, sortDirection: {}, filters: {}, dateRanges: {}",
                 size, number, sortBy, sortDirection, filters, dateRanges);
         Sort.Direction direction = sortDirection != null ? Sort.Direction.fromString(sortDirection) : Sort.Direction.DESC;
-        Sort sort = Sort.by(direction, sortBy != null ? sortBy : "startDate");
+        Sort sort = Sort.by(direction, sortBy != null ? sortBy : "controlDate");
         Pageable pageable = PageRequest.of(number, size, sort);
         Specification<Position> specification = filterUtil.buildSpecification(filters, dateRanges);
         Page<PositionResponse> page = positionRepository.findAll(specification, pageable).map(PositionMapper::toResponse);
